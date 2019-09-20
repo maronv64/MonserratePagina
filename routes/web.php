ed4dfb333baa35f8f80e11b46c9f125d8f48b80e
Tavo0928 <gustavo-828@hotmail.com>
2019-09-20 16:34:07 -0500

    cambios echos

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 Route::get('/social_form','NancyController@verSocial')->name('social_form');
-=======
 
 Route::get('/usuarios_form','MauroController@verUsuarios')->name('usuarios_form');
->>>>>>> c5e7361d3dfa8b5c1e05974a2ff8ae9a5051e44f
-=======
+
 Route::get('/estudiantes_form','GustavoController@verEstudiante')->name('estudiante_form');
->>>>>>> 57153a5a60998789d0a141671bbbef8b6bc52d96
+
