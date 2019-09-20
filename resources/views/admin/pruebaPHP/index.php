9ea9587cdcbdbfc0d3e166b1eefb1df768e17590
ElizabethCarreno <eli16.carrenochavez@gmail.com>
2019-09-20 15:42:51 -0500

    helous

---
+++ resources/views/admin/pruebaPHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hellous</title>
+</head>
+<body>
+    <h1> hola mundo</h1>
+    <?php
+        $cars = array( "hola ","niños");
+        $arrlength = count($cars);
+
+        for($x = 0; $x < 2; $x++) {
+            echo $cars[$x];
+            echo "<br>";
+        }
+        
+?>
+
+        
+    
+</body>
+</html>
