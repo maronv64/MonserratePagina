b465a86452d5a0f808cdfc20d3e857b7b6cfa69f
NancyVillamar <nancyvillamar01@gmail.com>
2019-09-20 16:23:50 -0500

    controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/social_form','NancyController@verSocial')->name('social_form');
