af9557cf9e9b47786d2e4ea63e64101d3f7dbb82
arath-darkport <santosmmauro24gmail.com>
2019-09-20 23:29:15 +0200

    as

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/usuarios_form','MauroController@verUsuarios')->name('usuarios_form');
