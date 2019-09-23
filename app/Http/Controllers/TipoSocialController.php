25384388dff2e3e4b9da066c5d1c0bfcd69d6414
NancyVillamar <nancyvillamar01@gmail.com>
2019-09-23 14:34:19 -0500

    g

---
+++ app/Http/Controllers/TipoSocialController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TipoSocial;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TipoSocialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TipoSocial  $tipoSocial
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoSocial $tipoSocial)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TipoSocial  $tipoSocial
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TipoSocial $tipoSocial)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TipoSocial  $tipoSocial
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoSocial $tipoSocial)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TipoSocial  $tipoSocial
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoSocial $tipoSocial)
+    {
+        //
+    }
+}
---
+++ database/factories/TipoSocialFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\TipoSocial;
+use Faker\Generator as Faker;
+
+$factory->define(TipoSocial::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
