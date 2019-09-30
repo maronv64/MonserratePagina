58ef299d7d9c073ae61ca778c68203ea2f2ce7d8
ElizabethCarreno <eli16.carrenochavez@gmail.com>
2019-09-30 16:45:19 -0500

    relaciones

--- app/Especialidades.php
+++ app/Especialidades.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Especialidades extends Model
 {
-    //
+    public function lista_materias()
+    {
+        return $this->hasMany('App\RelacionEspMat','id_especialidad','id')->with('materia');
+    }
 }
--- app/TipoPersonal.php
+++ app/TipoPersonal.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoPersonal extends Model
 {
-    //
+    public function lista_personal()
+    {
+        return $this->hasMany('App\RelacionPersTp','id_tipopersonal','id')->with('Personal');
+    }
 }
