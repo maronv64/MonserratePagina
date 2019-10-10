bcf800504480c6f97c10b8890d46e5d8487a0eb3
ElizabethCarreno <eli16.carrenochavez@gmail.com>
2019-10-10 09:43:02 -0500

    cambios

--- public/js/GestionEspecialidades.js
+++ public/js/GestionEspecialidades.js
@@ -1,3 +1,4 @@
+var listaidmateria=[];
 function verEspecialidad(id) {
     //alert("este es id :"+id);
    
@@ -12,7 +13,7 @@ function verEspecialidad(id) {
 function verMateriaE(id) {
     //  alert("este es id :"+id);
     $("#idEspecialidades_").val(id);
-
+    listaidmateria.length=0;
     $.get('Especialidades_control/'+id,function (data) {
         $.each(data.lista_materias,function (a,item) {
             // console.log(item);
@@ -21,10 +22,11 @@ function verMateriaE(id) {
                     //alert($(this).val());
                 
 
-                    console.log($(this).val());
+//console.log($(this).val());
                     if (item.materia.id==$(this).val()) {
                         $(this).attr('checked',true);
-                        listaidmateria.push(item.materia.id);
+                        listaidmateria.push(item.materia.id.toString());
+                       // console.log($(this).val());
                     }
                 // console.log( $(this).find("td:eq(2) input[type='checkbox']").val());
                 });
@@ -34,27 +36,36 @@ function verMateriaE(id) {
     });
     $(".especialidadMateria").modal("show");
 }
-var listaidmateria=[];
+
 function pasaridmateria(obj) 
 {
     var id=$(obj).val();
-
+//console.log(id);
     if(obj.checked == true){
 
         if (listaidmateria.length== 0) {//la lista esta limpia 
             listaidmateria.push(id);
-            console.log(listaidmateria);
+            //console.log(listaidmateria);
         } 
         if (listaidmateria.indexOf(id)=="-1") {//ese elemento no esta dentro de la lista
             listaidmateria.push(id);
-            console.log(listaidmateria);
+            //console.log(listaidmateria);
         } 
     } else if(obj.checked == false){
-        var indice = listaidmateria.indexOf(id);
-        listaidmateria.splice(indice, 1);
-        // console.log(listaidmateria);
+        console.log(id);
+        removeItemFromArr(listaidmateria,id);
+        //var indice = listaidmateria.indexOf(id);
+        //listaidmateria.splice(indice, 1);
+        console.log(listaidmateria);
     }
     // console.log(listaidtipo);
     $("#listaMateriasId").val(listaidmateria);
 //   alert(id);
+}
+function removeItemFromArr ( arr, item ) {
+    var i = arr.indexOf( item );
+
+    if ( i !== -1 ) {
+        arr.splice( i, 1 );
+    }
 }
--- public/js/adminSIS.js
+++ public/js/adminSIS.js
@@ -1,3 +1,10 @@
 $('.solonumeros').keyup(function (){
     this.value = (this.value + '').replace(/[^0-9]/g, '');
   });
+  function removeItemFromArr ( arr, item ) {
+    var i = arr.indexOf( item );
+
+    if ( i !== -1 ) {
+        arr.splice( i, 1 );
+    }
+}
